0a52355a760dd60dd6cca7dd838db5395e413ccd
antauren <antauren@yandex.ru>
2019-03-12 22:47:42 +0300

    Update gendiff.py

--- gendiff.py
+++ gendiff.py
@@ -3,9 +3,16 @@ import sys
 import difflib
 import json
 
-from utils import heandler
+from utils import heandler, is_args_correct
+
+
+def gendiff(file_1, file_2, format):
+    return 'test "{}" "{}" "{}"'.format(file_1, file_2, format)
+
 
 if __name__ == '__main__':
     args = sys.argv[1:]
 
-    print(heandler(args))
+    heandled_args = heandler(args)
+
+    print(heandled_args)
