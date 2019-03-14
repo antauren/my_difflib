68e464a715ac16a0027cc4812fb5cf04a662ba8f
antauren <antauren@yandex.ru>
2019-03-15 01:54:51 +0300

    Create step2.py

---
+++ step2.py
@@ -0,0 +1,38 @@
+'''docstring'''
+
+
+def make_row(sym, key, value):
+    '''pattern'''
+    return ' {} {}: {}'.format(sym, key, value)
+
+
+def get_diff(before: dict, after: dict) -> list:
+    '''diff'''
+    diff = []
+
+    for key in set(before) | set(after):
+
+        if key not in before:
+            row = make_row('+', key, after[key])
+            diff.append(row)
+
+        elif key not in after:
+            row = make_row('-', key, before[key])
+            diff.append(row)
+
+        elif before[key] == after[key]:
+            row = make_row(' ', key, before[key])
+            diff.append(row)
+
+        else:
+            row = make_row('-', key, before[key])
+            diff.append(row)
+
+            row = make_row('+', key, after[key])
+            diff.append(row)
+
+    return diff
+
+
+if __name__ == '__main__':
+    pass
