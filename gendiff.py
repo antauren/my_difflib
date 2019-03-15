14650bb8140520ffdbbc55f12669f4a92d372fb8
antauren <antauren@yandex.ru>
2019-03-15 04:48:51 +0300

    Update gendiff.py

--- gendiff.py
+++ gendiff.py
@@ -1,7 +1,9 @@
 import sys
 
-from utils import heandler, is_args_correct, json_load  # TODO
+from utils import heandler, is_args_correct  # TODO
 from step2 import get_diff
+
+from parsers import parse
 
 
 def gendiff(file_1, file_2, format):
@@ -15,8 +17,8 @@ if __name__ == '__main__':
 
     file_1, file_2 = heandled_args[:2]
 
-    before = json_load(file_1)
-    after = json_load(file_2)
+    before = parse(file_1)
+    after = parse(file_2)
 
     for row in get_diff(before, after):
         print(row)
